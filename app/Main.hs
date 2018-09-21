cc84614cffb24edf7faefc322b4a48f3c9926880
Giuseppe Capizzi <g.capizzi@gmail.com>
2018-09-21 17:36:25 +0100

    Hello, world!

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
+
+import qualified Network.Wai as Wai
+import qualified Network.Wai.Handler.Warp as Warp
+import qualified Network.HTTP.Types.Status as Status
 
 import Lib
 
+application :: Wai.Application
+application _ respond = respond $ Wai.responseLBS Status.status200 [] "Hello, World!"
+
 main :: IO ()
-main = someFunc
+main = Warp.run 8080 application
