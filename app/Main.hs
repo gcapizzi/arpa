33eff694145c2d47fa174611f80a24b05b800176
Giuseppe Capizzi <g.capizzi@gmail.com>
2018-09-22 13:59:31 +0100

    Refactoring

--- app/Main.hs
+++ app/Main.hs
@@ -7,10 +7,14 @@ import qualified Network.HTTP.Types.Status as Status
 
 import Lib
 
+fileResponse :: FilePath -> Wai.Response
+fileResponse path = Wai.responseFile Status.status200 [] path Nothing
+
+requestPath :: Wai.Request -> FilePath
+requestPath request = ByteString.unpack $ Wai.rawPathInfo request
+
 application :: Wai.Application
-application request respond = respond $ Wai.responseFile Status.status200 [] path Nothing
-  where
-    path = ByteString.unpack $ Wai.rawPathInfo request
+application request respond = respond $ fileResponse $ requestPath request
 
 main :: IO ()
 main = Warp.run 8080 application
