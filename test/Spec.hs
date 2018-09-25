535eaf31e6babdf9f8792757e0cdc46572921a62
Giuseppe Capizzi <g.capizzi@gmail.com>
2018-09-25 17:49:51 +0100

    First test!

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Test.Hspec
+
+import Network.HTTP.Client
+import Network.HTTP.Types.Status
+
+get manager path = do
+  req <- parseRequest $ "GET http://localhost:8080" ++ path
+  httpLbs req manager
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ beforeAll (newManager defaultManagerSettings) $
+  describe "arpa" $
+    context "when the requested file does not exist" $
+      it "returns a 404 response" $ \manager -> do
+        response <- get manager "/foo"
+
+        responseStatus response `shouldBe` notFound404
+        responseBody response `shouldBe` "File not found"
