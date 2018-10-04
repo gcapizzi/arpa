2639e4c80175b5b73b43a13421f914f7c2682488
Giuseppe Capizzi <g.capizzi@gmail.com>
2018-10-04 17:30:21 +0100

    Serve files ourselves instead of using Wai.responseFile

--- src/Application.hs
+++ src/Application.hs
@@ -10,25 +10,45 @@ import qualified Data.ByteString.Lazy as ByteStringLazy
 import qualified Data.ByteString.Lazy.Char8 as ByteStringLazyChar8
 import qualified Network.Wai as Wai
 import qualified Network.HTTP.Types.Status as Status
+import qualified System.Directory as Directory
 
 application :: Wai.Application
-application request respond = respond $ maybe invalidPathResponse fileResponse (fsPath request)
+application request respond = handle request >>= respond
+
+handle :: Wai.Request -> IO Wai.Response
+handle request = maybe (return invalidPathResponse) fileResponse (requestPath request)
 
 invalidPathResponse :: Wai.Response
 invalidPathResponse = Wai.responseLBS Status.badRequest400 [] (ByteStringLazyChar8.pack "Invalid path")
 
-fileResponse :: FilePath -> Wai.Response
-fileResponse path = Wai.responseFile Status.ok200 [] path Nothing
+fileResponse :: FilePath -> IO Wai.Response
+fileResponse path = do
+  absolutePath <- Directory.makeAbsolute path
+  fileExists <- Directory.doesFileExist absolutePath
+  if fileExists
+  then
+    okResponse <$> ByteStringLazy.readFile absolutePath
+  else
+    return notFoundResponse
 
-fsPath :: Wai.Request -> Maybe FilePath
-fsPath request
-  | hasParentDir requestPath = Nothing
-  | otherwise = Just $ "." ++ requestPath
+okResponse :: ByteStringLazy.ByteString -> Wai.Response
+okResponse = Wai.responseLBS Status.ok200 []
+
+notFoundResponse :: Wai.Response
+notFoundResponse = Wai.responseLBS Status.notFound404 [] (ByteStringLazyChar8.pack "File not found")
+
+requestPath :: Wai.Request -> Maybe FilePath
+requestPath request
+  | hasParentDir relativePath = Nothing
+  | otherwise = Just relativePath
   where
-    requestPath = rawPath request
+    relativePath = dropRoot $ rawPath request
 
 rawPath :: Wai.Request -> FilePath
 rawPath request = ByteStringChar8.unpack $ Wai.rawPathInfo request
 
 hasParentDir :: FilePath -> Bool
 hasParentDir = isInfixOf "../"
+
+dropRoot :: FilePath -> FilePath
+dropRoot = drop 1
