a32c2828db9dc1492ff1438815e44a28446a18fe
Giuseppe Capizzi <g.capizzi@gmail.com>
2018-10-05 18:29:54 +0100

    Move path validation to the Render module

--- src/Application.hs
+++ src/Application.hs
@@ -2,10 +2,6 @@ module Application
     ( application
     ) where
 
-import Data.Foldable (find)
-import Data.Maybe (maybe)
-import Data.List (isInfixOf)
-
 import qualified Data.ByteString.Char8 as ByteStringChar8
 import qualified Data.ByteString.Lazy as ByteStringLazy
 import qualified Data.ByteString.Lazy.Char8 as ByteStringLazyChar8
@@ -18,10 +14,7 @@ application :: Wai.Application
 application request respond = handle request >>= respond
 
 handle :: Wai.Request -> IO Wai.Response
-handle request = maybe (return invalidPathResponse) fileResponse (requestPath request)
-
-invalidPathResponse :: Wai.Response
-invalidPathResponse = Wai.responseLBS Status.badRequest400 [] (ByteStringLazyChar8.pack "Invalid path")
+handle request = fileResponse (rawPath request)
 
 fileResponse :: FilePath -> IO Wai.Response
 fileResponse path = do
@@ -32,21 +25,11 @@ okResponse :: ByteStringLazy.ByteString -> Wai.Response
 okResponse = Wai.responseLBS Status.ok200 []
 
 errorResponse :: Render.Error -> Wai.Response
-errorResponse _ = notFoundResponse
+errorResponse Render.FileNotFound = responseString Status.notFound404 "File not found"
+errorResponse Render.InvalidPath = responseString Status.badRequest400 "Invalid path"
 
-notFoundResponse :: Wai.Response
-notFoundResponse = Wai.responseLBS Status.notFound404 [] (ByteStringLazyChar8.pack "File not found")
-
-requestPath :: Wai.Request -> Maybe FilePath
-requestPath request = find isValidPath (Just relativePath)
-  where
-    relativePath = dropRoot $ rawPath request
+responseString :: Status.Status -> String -> Wai.Response
+responseString status body = Wai.responseLBS status [] (ByteStringLazyChar8.pack body)
 
 rawPath :: Wai.Request -> FilePath
 rawPath request = ByteStringChar8.unpack $ Wai.rawPathInfo request
-
-isValidPath :: FilePath -> Bool
-isValidPath = not . isInfixOf "../"
-
-dropRoot :: FilePath -> FilePath
-dropRoot = drop 1
--- src/Render.hs
+++ src/Render.hs
@@ -1,19 +1,29 @@
 module Render
   ( renderFile
-  , Error
+  , Error(..)
   ) where
 
+import Data.List (isInfixOf)
 import qualified Data.ByteString.Lazy as ByteStringLazy
 import qualified System.Directory as Directory
 
-data Error = Error
+data Error = FileNotFound
+           | InvalidPath
 
 renderFile :: FilePath -> IO (Either Error ByteStringLazy.ByteString)
-renderFile path = do
-  absolutePath <- Directory.makeAbsolute path
-  fileExists <- Directory.doesFileExist absolutePath
-  if fileExists
-  then
-    Right <$> ByteStringLazy.readFile absolutePath
-  else
-    return $ Left Error
+renderFile path
+  | isValidPath path = do
+    absolutePath <- Directory.makeAbsolute (dropRoot path)
+    fileExists <- Directory.doesFileExist absolutePath
+    if fileExists
+    then
+      Right <$> ByteStringLazy.readFile absolutePath
+    else
+      return $ Left FileNotFound
+  | otherwise = return $ Left InvalidPath
+
+isValidPath :: FilePath -> Bool
+isValidPath = not . isInfixOf "../"
+
+dropRoot :: FilePath -> FilePath
+dropRoot = drop 1
